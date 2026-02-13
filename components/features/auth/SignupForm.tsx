adf9ee4b3afe64fde527b53f9644989237f7f967
eunnn <ehddms0517@naver.com>
2026-02-13 19:41:48 +0900

    feat: 관심분야 추가

--- components/features/auth/SignupForm.tsx
+++ components/features/auth/SignupForm.tsx
@@ -1,13 +1,25 @@
+import { useState } from 'react';
 import BaseButton from '@/components/shared/BaseButton';
 import AuthSection from './AuthSection';
 import ProfileSection from './ProfileSection';
+import InterestSection from './InterestSection';
 
 export default function SignupForm() {
+  const [major, setMajor] = useState<string>('');
+  const [minor, setMinor] = useState<string>('');
+
   return (
     <form className="flex flex-col gap-5 w-full">
       <AuthSection />
 
       <ProfileSection />
+
+      <InterestSection
+        major={major}
+        minor={minor}
+        onChangeMajor={setMajor}
+        onChangeMinor={setMinor}
+      />
 
       <BaseButton size="L" full={true} type="submit">
         <span className="font-bold">가입하기</span>
---
+++ constants/interest.ts
@@ -0,0 +1,13 @@
+export const OPTIONS = [
+  { major: '기획', minor: ['프로덕트 매니저/오너'] },
+  {
+    major: '디자인',
+    minor: ['그래픽디자인', 'UI/UX디자인', '모션 디자인'],
+  },
+  {
+    major: '프론트엔드',
+    minor: ['웹프론트엔드', 'iOS', '안드로이드', '크로스플랫폼'],
+  },
+  { major: '백엔드', minor: ['웹서버', 'AI'] },
+  { major: '기타', minor: ['기타'] },
+];
